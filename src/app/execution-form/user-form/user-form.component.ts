311c430ce2c71cc7912537ce74dcb1f3cf5928d6
Camilo Flores <camilo.ignacio@gmail.com>
2016-10-18 20:18:08 -0300

    [task] Refactor forms to use bootstrap v4

--- src/app/execution-form/user-form/user-form.component.ts
+++ src/app/execution-form/user-form/user-form.component.ts
@@ -7,53 +7,11 @@ import { NgbDatepickerService } from '@ng-bootstrap/ng-bootstrap/datepicker/date
 import { NgbCalendar } from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-calendar';
 import { RutValidator } from 'ng2-rut';
 
-import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
-
-const cities = [
-  'Santiago',
-  'Concepción',
-  'Valparaíso',
-  'La Serena',
-  'Temuco',
-  'Antofagasta',
-  'Iquique',
-  'Rancagua',
-  'Puerto Montt',
-  'Talca',
-  'Arica',
-  'Chillán',
-  'Los Ángeles',
-  'Copiapó',
-  'Quillota',
-  'Valdivia',
-  'Osorno',
-  'San Antonio',
-  'Curicó',
-  'Calama',
-  'Punta Arenas',
-  'Colina',
-  'Melipilla',
-  'Ovalle',
-  'Linares',
-  'Peñaflor',
-  'Lampa',
-  'Buin',
-  'San Fernando',
-  'San Felipe',
-  'Paine',
-  'Talagante',
-  'Los Andes',
-  'Coyhaique',
-  'Rengo',
-  'Vallenar',
-  'Villarrica',
-  'San Carlos',
-  'Angol'
- ]
-
+import { Observable } from 'rxjs/Observable';
+import { cities } from '../../shared/CL-cities';
 
 @Component({
   selector: 'user-form',
@@ -69,26 +27,28 @@ export class UserFormComponent implements AfterViewInit {
   constructor (
     private fb:FormBuilder,
     private renderer:Renderer,
-    private config:NgbDatepickerConfig,
+    private datePickerConfig:NgbDatepickerConfig,
     private rutValidator:RutValidator) {
 
-    config.startDate = {year: 1990, month: 3}
+    datePickerConfig.minDate = { year: 1910, month: 3, day: 1 };
+    datePickerConfig.maxDate = { year: 1998, month: 11, day: 30 };
 
     let emailRegex = `([a-zA-Z0-9_.]{1}[a-zA-Z0-9_.]*)((@[a-zA-Z]{2}[a-zA-Z]*)[\\\.]([a-zA-Z]{2}|[a-zA-Z]{3}))`;
     let dateRegex = `^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$`;
     let zipCodeRegex = `[0-9]{6,8}`
+    let phoneRegex = `[0-9]{8,10}`
 
     this.userForm = fb.group({
       'firstName': ['', [Validators.required, Validators.minLength(2)]],
-      'lastName': ['', [Validators.required]],
+      'lastName': ['', [Validators.required, Validators.minLength(2)]],
       'email': ['', [Validators.required, Validators.pattern(emailRegex)]],
-      'birthDate': ['', [Validators.required, Validators.pattern(dateRegex)]],
+      'birthDate': ['', [Validators.required]],
       'address': ['', [Validators.required, Validators.minLength(5)]],
       'city': ['', [Validators.required]],
       'zipCode': ['', [Validators.required, Validators.pattern(zipCodeRegex)]],
-      'phone': ['', [Validators.required]],
+      'phone': ['', [Validators.required, Validators.pattern(phoneRegex)]],
       'country': [{value: 'Chile', disabled: true}, [Validators.required]],
-      'rut': ['157356399', [Validators.required]]
+      'rut': ['', [Validators.required]]
     });
   }
 
@@ -102,9 +62,19 @@ export class UserFormComponent implements AfterViewInit {
 
   search (text: Observable<string>) {
     return text
-      .debounceTime(300)
+      .debounceTime(100)
       .distinctUntilChanged()
       .map(term => term.length < 1 ? []
         : cities.filter(v => new RegExp(term, 'gi').test(v)).splice(0, 10));
   }
 }
+
+
+// export class Validators {
+//   /**
+//    * Validator that requires controls to have a non-empty value.
+//    */
+//   static required(control: AbstractControl): {[key: string]: boolean} {
+//     return isEmptyInputValue(control.value) ? {'required': true} : null;
+//   }
+// }
